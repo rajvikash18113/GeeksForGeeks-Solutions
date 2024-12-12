5cd251ada42e65723e88d8e3f218b5756ee52732
Vikash Kumar <rajvikash18113@gmail.com>
2024-12-12 12:05:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Switch Case statement/java-switch-case-statement.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%lf ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        cout<<setprecision(3)<<fixed;
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    double switchCase(int choice, vector<double> &arr) {
+        // Switch case for handling different choices
+        switch (choice) {
+            case 1: // Area of a circle
+                if (arr.size() != 1) return -1; // Validate array size
+                return M_PI * arr[0] * arr[0];
+
+            case 2: // Area of a rectangle
+                if (arr.size() != 2) return -1; // Validate array size
+                return arr[0] * arr[1];
+
+            default: // Handle invalid choice
+                return -1; // Return -1 for invalid choice
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int choice;
+        scanf("%d",&choice);
+        
+        
+        vector<double> arr(choice);
+        Array::input(arr,choice);
+        
+        Solution obj;
+        double res = obj.switchCase(choice, arr);
+        // cout<<fixed<<endl;
+        if(res == int(res)){
+            cout<<int(res)<<endl;
+            continue;
+        }
+        cout<<setprecision(2)<<fixed;
+        // 
+        // }else{
+            cout<<res<<endl;
+        // }
+        
+    }
+}
+
+// } Driver Code Ends

6f92f6a2113e2c92bf8999db0464eb4ffad0c25b
Vikash Kumar <rajvikash18113@gmail.com>
2025-02-16 22:09:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+  int sum = 0;
+  int count = 1;
+    int sumOfSeries(int n) {
+        if(count > n)
+            return sum;
+        sum += (count * count * count);
+        count++;
+        sumOfSeries(n);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+}
+// } Driver Code Ends

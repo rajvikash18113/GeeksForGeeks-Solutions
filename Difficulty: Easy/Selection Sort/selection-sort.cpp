2fe765ea15ff9f7dd6731516e5d8834b06cf1eae
Vikash Kumar <rajvikash18113@gmail.com>
2025-03-04 16:30:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.cpp
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -31,7 +31,7 @@ class Solution {
         for(int i = 0; i <= n - 2; i++)
         {
             int mini = i;
-            for(int j = i; j <= n - 1; j++)
+            for(int j = i + 1; j <= n - 1; j++)
             {
                 if(arr[j] < arr[mini])
                 {

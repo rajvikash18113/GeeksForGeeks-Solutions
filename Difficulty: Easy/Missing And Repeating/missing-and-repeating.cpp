f057b11c23eaf7d8c3056d4affde65ee41fe05bb
Vikash Kumar <rajvikash18113@gmail.com>
2025-07-13 15:49:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.cpp
@@ -3,7 +3,7 @@ class Solution {
     vector<int> findTwoElement(vector<int>& arr) {
         vector<int> ans(2);
         for(int i = 0; i < arr.size(); i++){
-            if(arr[abs(arr[i])-1] > 0){
+            if(arr[abs(arr[i])-1] >= 0){
                 arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
             }
             else{

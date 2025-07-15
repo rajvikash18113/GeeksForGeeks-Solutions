7a47b1e427979e0965fdeb8d5a973e6659be0fcd
Vikash Kumar <rajvikash18113@gmail.com>
2025-07-15 22:57:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
@@ -1,8 +1,25 @@
 class Solution {
   public:
-  //---------C++ Stl to find the lower bound element in an array-------//
+  
     int lowerBound(vector<int>& arr, int target) {
-        int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
-        return lb;
+        //----C++ STL to find Lower Bound of an Array----//
+        // int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
+        // return lb;
+        
+        //----Internal working of C++ STL to find Lower Bound----//
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };

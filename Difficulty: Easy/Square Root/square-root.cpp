b997670f27bbf248f197d8f270e60cef020a2006
Vikash Kumar <rajvikash18113@gmail.com>
2025-08-20 22:00:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int ans = -1;
+        int low = 1, high = n;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(mid * mid <= n){
+                ans = mid;
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
